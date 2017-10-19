75ba5c18abb1b9c13b92c612a45380f4a7883ef9
Blake Pfaff <blake.a.pfaff2@gmail.com>
2017-10-19 12:16:56 -0500

    adds Question and Answer Models & tests (RSpec issue)

--- db/migrate/20171019170232_create_answers.rb
+++ db/migrate/20171019170232_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[5.1]
   def change
     create_table :answers do |t|
       t.text :body
-      t.refrences :question
+      t.references :question, foreign_key: true
 
       t.timestamps
     end
