1a4c971c9fcf2d6f641a17824edee6528c124185
Blake Pfaff <blake.a.pfaff2@gmail.com>
2017-10-23 21:05:26 -0500

    adds Ads model, Ctrl & view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post= Post.new
+    @post = Post.new
   end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ require 'random_data'
     body:   RandomData.random_paragraph
   )
 end
+
 posts = Post.all
 
 # Create Comments
@@ -17,9 +18,20 @@ posts = Post.all
   )
 end
 
+10.times do
+  Advertisement.create!(
+    title:  RandomData.random_sentence,
+    copy:   "THIS IS THE COPY"
+    price:
+
+end
+
+advertisements = Advertisement.all
+
 Post.find_or_create_by(title:"test title", body:"test body")
 Comment.find_or_create_by(body:"test comment body")
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} Advertisements created"
