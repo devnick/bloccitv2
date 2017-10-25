8321225a1ae4abab0d15ba10173f6d7048bc7cfe
Blake Pfaff <blake.a.pfaff2@gmail.com>
2017-10-25 17:56:11 -0500

    adds ability to create sponsered postes w/ creat method in Ctrl

--- app/controllers/sponsored_posts_controller.rb
+++ app/controllers/sponsored_posts_controller.rb
@@ -13,7 +13,19 @@ class SponsoredPostsController < ApplicationController
   end
 
   def create
+    @sponsored_post = SponsoredPost.new
+    @sponsored_post.title = params[:sponsored_post][:title]
+    @sponsored_post.body = params[:sponsored_post][:body]
+    @topic = Topic.find(params[:topic_id])
+    @sponsored_post.topic = @topic
 
+    if @sponsored_post.save
+      flash[:notice] = "Sponsored Post was saved."
+      redirect_to [@topic, @sponsored_post]
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 
 end
